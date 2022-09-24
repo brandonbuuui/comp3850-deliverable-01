b96531304d5b73a52848d8bd54903f459766aa6e
BrandonBuiMQ202046494456 <brandon.bui@students.mq.edu.au>
2022-09-24 18:20:51 +1000

    readd app.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './app/navigation/StackNavigator';
+import TabNavigator from './app/navigation/TabNavigator';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createRoot } from 'react-dom/client'
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StackNavigator/>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
