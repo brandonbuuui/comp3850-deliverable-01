239b4f6a06fa446a92fb1c76a55994b72b23d579
Nguyen Vo <nguyen.vo@students.mq.edu.au>
2022-10-11 23:28:01 +1100

    survey screen designs

--- app/screens/SurveyListScreen.js
+++ app/screens/SurveyListScreen.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { NavigationContainer, NavigationHelpersContext } from '@react-navigation/native';
+import { View, StyleSheet, Text, Image } from 'react-native';
+import DataManager from '../config/DataManager';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+import AppColours from '../config/AppColours';
 
 function SurveyListScreen({navigation}) {
+    let data = DataManager.getInstance();
+    let currUser = data.getCurrUser();
+    console.log(currUser.email)
     return (
         <View style = {styles.container}>
-            <Text>SURVEY LIST SCREEN</Text>
+             <View style = {styles.header}>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate("Profile")}>
+                          <Image
+                            style = {styles.profileButton}
+                            source={currUser.image}>
+                        </Image>
+                </TouchableOpacity>
+                </View>
+            <View style={styles.surveyContainer}>
+                <View style={styles.title}>
+                    <Text style={{marginTop: 20, fontSize: 30, fontWeight: 'bold'}}>Today's Surveys</Text>
+                </View>
+                <View style={styles.surveyLists}></View>
+            </View>
         </View>
     );
 }
@@ -14,8 +35,37 @@ const styles = StyleSheet.create ({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+    },
+    profileButton: {
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        height: 64, 
+        width: 64, 
+        borderRadius: 75, 
+        borderColor: AppColours.lightblue, 
+        borderWidth: 5,
+        marginRight: 20
+    },
+    header: {
+        marginTop: 20,
+        flexDirection: 'row-reverse',
+    },
+    surveyContainer: {
+        flex: 2,
+        width: "95%",
+    },
+    title: {
+        marginLeft: 10,
+    },
+    surveyLists: {
+        flex: 1,
+        marginTop: 10,
+        marginLeft: 10,
+        width: "100%",
+        alignContent: 'center',
+        backgroundColor: AppColours.purple,
+        borderRadius: 10,
+        padding: 10,
     }
 });
 
